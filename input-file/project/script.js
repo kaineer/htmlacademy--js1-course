bdfde035d31ecca25af24590398b9ff4a58c197a
Sergey Kluchkovsky <kaineer@gmail.com>
2024-05-07 12:52:52 +0500

    Позволяет добавлять файлы к списку

--- input-file/project/script.js
+++ input-file/project/script.js
@@ -1,35 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const fileInput = document.querySelector("input[type='file']");
-  const fileList = document.querySelector(".file-list");
+const createFileList = (input, container) => {
+  const data = {
+    files: [],
+  };
 
-  const redrawFileList = () => {
-    fileList.innerHTML = "";
-    let content = "";
-    for (const file of fileInput.files) {
-      const item = "<li>" + file.name + " <a href='' data-file='" + file.name + "'>x</a></li>";
-      content += item;
+  const pullFiles = (dt, source, cond = (x) => true) => {
+    for (const file of source) {
+      if (cond(file)) {
+        dt.items.add(file);
+      }
     }
-    fileList.innerHTML = content;
   }
 
-  fileList.addEventListener("click", (e) => {
+  const commitDT = (dt) => {
+    input.files = dt.files;
+    data.files = [...dt.files];
+  }
+
+  const itemTemplate = (file) => {
+    return (
+      "<li>" + file.name + " <a href='' data-file='" +
+      file.name + "'>x</a></li>"
+    );
+  }
+
+  return {
+    add(files) {
+      const dt = new DataTransfer();
+      pullFiles(dt, data.files);
+      pullFiles(dt, files);
+      commitDT(dt);
+    },
+    remove(fileName) {
+      const dt = new DataTransfer();
+      pullFiles(dt, data.files, (f) => f.name !== fileName);
+      commitDT(dt);
+    },
+    render() {
+      let content = "";
+      for (const file of data.files) {
+        content += itemTemplate(file);
+      }
+      container.innerHTML = content;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const fileInput = document.querySelector("input[type='file']");
+  const fileListEl = document.querySelector(".file-list");
+  const fl = createFileList(fileInput, fileListEl);
+
+  fileListEl.addEventListener("click", (e) => {
     e.preventDefault();
+
     const t = e.target;
     if (t.tagName.toLowerCase() === "a") {
-      const dt = new DataTransfer();
-      for (const file of fileInput.files) {
-        if (file.name !== t.dataset.file) {
-          dt.items.add(file);
-        }
-      }
-
-      fileInput.files = dt.files;
-
-      redrawFileList();
+      fl.remove(t.dataset.file);
+      fl.render();
     }
   });
 
-  fileInput.addEventListener("change", (e) => {
-    redrawFileList();
+  fileInput.addEventListener("change", function (e) {
+    fl.add(this.files);
+    fl.render();
   });
 });
